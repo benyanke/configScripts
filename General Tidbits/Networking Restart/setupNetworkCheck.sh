e5892180dcc2c2a8d8d094d6f4113d0b2d1b5763
Ben Yanke <ben@benyanke.com>
2016-03-01 22:29:05 -0600

    Update setupNetworkCheck.sh

--- General Tidbits/Networking Restart/setupNetworkCheck.sh
+++ General Tidbits/Networking Restart/setupNetworkCheck.sh
@@ -11,6 +11,8 @@ touch "/home/benyanke/scripts/restartNetwork.sh"
 
 wget "https://raw.githubusercontent.com/benyanke/configScripts/master/General%20Tidbits/Networking%20Restart/restartNetwork.sh"  -O "/home/benyanke/scripts/restartNetwork.sh"
 
+chmod 111 "/home/benyanke/scripts/restartNetwork.sh"
+
 crontab -l | { cat; echo "* * * * * /bin/bash /home/benyanke/scripts/restartNetwork.sh >/dev/null 2>&1"; } | crontab -
 
 echo "Successfully added network checker and scheduled to run every minute";

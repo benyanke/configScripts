c1e1ae26745ab18c04e5bb5ad03f7e398ed5186b
Ben Yanke <ben@benyanke.com>
2016-03-03 23:23:30 -0600

    fix of script for proper permissions

--- Webserver Scripts/properWebDirPermissions.sh
+++ Webserver Scripts/properWebDirPermissions.sh
@@ -9,5 +9,4 @@ fi
 USER=benyanke
 sudo adduser $USER www-data
 sudo chown $USER:www-data -R /var/www
-#sudo chmod u=rwX,g=srX,o=rX -R /var/www
-sudo chmod 664 -R /var/www
+sudo chmod u=rwX,g=rwX,o=rX -R /var/www

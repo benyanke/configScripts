5a2a12489055885fcaa2579afe37a2ad7125b28a
Ben Yanke <ben@benyanke.com>
2016-03-03 23:31:17 -0600

    added features

---
+++ Webserver Scripts/apacheSetupTweaks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# todo:
+# set proper permissions in web directory
+# add link to home for www
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Set vars
+USER=benyanke
+WEBDIR=/var/www
+
+# Set proper permissions
+adduser $USER $WEBDIR
+chown $USER:www-data -R $WEBDIR
+chmod u=rwX,g=rwX,o=rX -R $WEBDIR
+
+# Add link to home directory
+ln -s  $WEBDIR /home/$USER/www

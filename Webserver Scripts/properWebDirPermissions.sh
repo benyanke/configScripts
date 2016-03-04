2eaf7128c8f795b42da8fb4608088aea3275269d
Ben Yanke <ben@benyanke.com>
2016-03-03 23:06:53 -0600

    Update properWebDirPermissions.sh

--- Webserver Scripts/properWebDirPermissions.sh
+++ Webserver Scripts/properWebDirPermissions.sh
@@ -9,4 +9,5 @@ fi
 USER=benyanke
 sudo adduser $USER www-data
 sudo chown $USER:www-data -R /var/www
-sudo chmod u=rwX,g=srX,o=rX -R /var/www
+#sudo chmod u=rwX,g=srX,o=rX -R /var/www
+sudo chmod 664 -R /var/www

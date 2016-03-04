7613355d061a5e5d1a9fbb12e850d55b15b0277b
Ben Yanke <ben@benyanke.com>
2016-03-03 23:05:24 -0600

    adding web permissions script

---
+++ Webserver Scripts/properWebDirPermissions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+USER=benyanke
+sudo adduser $USER www-data
+sudo chown $USER:www-data -R /var/www
+sudo chmod u=rwX,g=srX,o=rX -R /var/www

c93df4a2d4e18646640e8a35477916b6276edbec
Ben Yanke <ben@benyanke.com>
2016-06-05 10:40:19 -0500

    edit swap file script

--- Webserver Scripts/setupSwapFile.sh
+++ Webserver Scripts/setupSwapFile.sh
@@ -5,5 +5,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-swapFileSize=3
+# Default size in gb
+swapFileSize="4G"
+clear;
+echo "Creating empty swap file at $swapFileSize:"
 
+fallocate -l $swapFileSize /swapfile
+
+# set proper permissions
+chmod 600 /swapfile
+
+# make swap file
+mkswap /swapfile
+
+echo ; 
+echo ;
+
+echo "Check to see if swap is enabled: ";
+swapon -s
+echo "Press any key to continue"
+read dump;
+
+"\n/swapfile   none    swap    sw    0   0" >> /etc/fstab

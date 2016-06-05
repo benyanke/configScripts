81dde9bdfb68caaf4466b90a0ac49a72d2d009c2
Ben Yanke <ben@benyanke.com>
2016-06-05 13:20:07 -0500

    add vps checklist

---
+++ basicVpsSetupList.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+# Set up firewall
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw --force enable
+
+
+# Updating software
+apt-get update
+apt-get upgrade -y
+
+# Installing handy tools
+apt-get install git htop tree -y
+
+clear;
+echo "Run complete. Things you need to do still:";
+echo "";
+echo " * Setup backups"
+echo " * Fail2Ban"
+echo " * Add benyanke user"
+echo " * Add public key(s) for benyanke"
+echo " * Add to logging system"

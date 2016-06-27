db6ce75c4de6ee90bbda2529ddbdd33c74a2e0ee
Ben Yanke <ben@benyanke.com>
2016-06-26 23:47:00 -0500

    Update basicVpsSetupList.sh

--- basicVpsSetupList.sh
+++ basicVpsSetupList.sh
@@ -20,7 +20,7 @@ apt-get update
 apt-get upgrade -y
 
 # Installing handy tools
-apt-get install git htop tree notify-send boxes -y
+apt-get install git htop tree -y
 
 clear;
 echo "Run complete. Things you need to do still:";

cc3395f993c6cd301d7385e2d0f13de9064ff725
Ben Yanke <ben@benyanke.com>
2016-07-06 08:41:20 -0500

    Update thinkpadFingerprintEnable.sh

--- thinkpadFingerprintEnable.sh
+++ thinkpadFingerprintEnable.sh
@@ -5,11 +5,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+
+# GUI VERSION
+sudo apt-add-repository -y ppa:fingerprint/fingerprint-gui
+sudo apt-get update
+sudo apt-get install libbsapi policykit-1-fingerprint-gui fingerprint-gui -y
+
+# from http://www.omgubuntu.co.uk/2013/03/how-to-get-your-fingerprint-reader-working-in-ubuntu
+
+
+exit
+### OR CLI VERSION
+
 add-apt-repository -y ppa:fingerprint/fprint
 apt-get update
 apt-get install libfprint0 fprint-demo libpam-fprintd -y
 
 
+
 # Then run fprintd-enroll
 
 # from: http://askubuntu.com/questions/511876/how-to-install-a-fingerprint-reader-on-lenovo-thinkpad
+

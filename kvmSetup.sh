4780797008873378f5fa385091bf1506e851365d
Ben Yanke <ben@benyanke.com>
2016-08-03 13:31:07 -0500

    Update kvmSetup.sh

--- kvmSetup.sh
+++ kvmSetup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# From https://www.howtoforge.com/tutorial/kvm-on-ubuntu-14.04/
 
 # Install packages
 apt update
 apt install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
 
+# Add User
+sudo adduser `id -un` libvirtd
+
+# Setup Gui Tool
+sudo apt install -y virt-manager
+
+clear;
+echo ""
+echo "Looks like KVM setup is complete. Try running 'virt-manager' to use the gui."

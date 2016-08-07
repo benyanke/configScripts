2da4f5c5cbcb9d8db8628b7aa0b8abecd598d2bb
Ben Yanke <ben@benyanke.com>
2016-08-07 15:34:32 -0500

    Update kvmSetup.sh

--- kvmSetup.sh
+++ kvmSetup.sh
@@ -2,12 +2,16 @@
 
 # From https://www.howtoforge.com/tutorial/kvm-on-ubuntu-14.04/
 
+
+currentUser=$(who -m | awk '{print $1;}');
+
 # Install packages
 apt update
 apt install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
 
 # Add User
-adduser `id -un` libvirtd
+adduser $currentUser libvirtd
+adduser $currentUser kvm
 
 # Setup Gui Tool
 apt install -y virt-manager

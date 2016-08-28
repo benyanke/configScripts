7ea2e446d45e1df98a6f2d5fdaafdc238bb02938
Ben Yanke <ben@benyanke.com>
2016-08-28 14:52:43 -0500

    Update ApacheProvisioning.sh

--- Webserver Scripts/ApacheProvisioning/ApacheProvisioning.sh
+++ Webserver Scripts/ApacheProvisioning/ApacheProvisioning.sh
@@ -3,6 +3,6 @@
 #
 # Processes to implement:
 #
-# Provision new site to pool (including SSL certs)
-# Enable site from pool
-# Disable site from pool
+# Provision new site to pool (create config, create www dir, restart server, obtain SSL certs, restart server, notify user)
+# Enable site from pool (symlink, restart server)
+# Disable site from pool (rm symlink, restart server)

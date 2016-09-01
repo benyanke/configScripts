e141ed771777432a60c47e2eceec9eea7ecdfdbe
Ben Yanke <ben@benyanke.com>
2016-09-01 11:42:06 -0500

    Update settingsDigger.sh

--- settingsDigger.sh
+++ settingsDigger.sh
@@ -22,16 +22,15 @@ echo "Find the link between options in GUI and gsettings"
 echo ""
 
 # write old settings to file
-echo "Capturing current configuration before change"
 gsettings list-recursively > $old
 
 # make change
 read -rsp $'Make the change you want to detect and press any key to continue...\n' -n1 key
 
 # write new settings to file
-echo "Capturing current configuration after change"
 gsettings list-recursively > $new
 
 # look at difference between files
+echo ""
 echo "Here are the settings that have changed: "
 diff $old $new

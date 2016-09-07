fcb573c3c3a75fe220a2c3f2ce0dca156a03ca79
Ben Yanke <ben@benyanke.com>
2016-09-06 21:31:10 -0500

    Update and rename ChangeHostname.sh to ServerHostnameSetup.sh

---
+++ ServerHostnameSetup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+
+# Root check
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# get current user
+user=${SUDO_USER:-$(whoami)}
+
+# This file will exist in the filesystem of the unconfigured server VMs.
+flagFile="/tmp/IsServerUnconfigured"
+
+# check for flag in temporary directory
+if [ -a $flagFile ]; then
+
+        clear;
+        echo "#######################";
+        echo "New Server Setup Script";
+        echo "#######################";
+
+else
+        echo "This server appears to be already configured. Not sure why you're running $
+        read -n1 -r -p "Would you like to continue? [Y/N] " key
+        echo " "
+
+                if [ $key = "Y" ] || [ $key = "y" ]; then
+                        echo " ";
+                elif [ $key = "N" ] || [ $key = "n" ]; then
+                        echo "Exiting.";
+                        exit 0;
+                else
+                        echo "Not a recognized option. Exiting without modification.";
+                        exit 1;
+                fi
+fi
+
+
+OLD_HOSTNAME="$( hostname )"
+NEW_HOSTNAME="$1"
+
+if [ -z "$NEW_HOSTNAME" ]; then
+ echo -n "Please enter new hostname: "
+ read NEW_HOSTNAME < /dev/tty
+fi
+
+if [ -z "$NEW_HOSTNAME" ]; then
+ echo "Error: no hostname entered. Exiting."
+ exit 1
+fi
+
+echo "Changing hostname from $OLD_HOSTNAME to $NEW_HOSTNAME..."
+
+
+
+# change hostname on system
+hostname $NEW_HOSTNAME
+
+# Change name in hosts file
+cat /etc/hosts | sed s/$OLD_HOSTNAME/$NEW_HOSTNAME/ > /tmp/newhosts
+mv /tmp/newhosts /etc/hosts
+
+# Change name in hostname file
+echo $NEW_HOSTNAME > /etc/hostname
+
+# Remove flag, marking server as fully configured
+rm $flagFile  2> /dev/null
+
+echo "Successfully changed. Restarting now. Press any key to continue."
+read nul
+
+shutdown -r now

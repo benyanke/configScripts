d8e94b789d75715fb0c1e5eef46d91aa0431c253
Ben Yanke <ben@benyanke.com>
2016-09-10 15:23:51 -0500

    Create reownWebDir.sh

---
+++ Webserver Scripts/reownWebDir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add user to www-data
+# usermod -a -G www-data benyanke
+
+# Ensure all files owned by www-data
+sudo chown -R www-data:www-data /var/www
+
+# modify all file permissions to proper
+sudo find /var/www -type d -exec chmod 0775 {} \;
+sudo find /var/www -type f -exec chmod 0664 {} \;
+

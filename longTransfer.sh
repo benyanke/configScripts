4a60721522eb0ae007bf0389034a0f73a9903aab
Ben Yanke <ben@benyanke.com>
2017-03-02 21:59:30 -0600

    updated longtran script

--- longTransfer.sh
+++ longTransfer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Long transfer script for rsync
-# Allow it to run in the background, and even if interrupted, it will retry again and again
+# Allow it to run in the background, and even if interrupted,
+# it will retry again and again
+
+# Known issue: it doesn't exit properly with CTRL + C, because this
+# kills the rsync process, which returns an error and continues in the loop.
 
 # Example:
 from="john@example.com:/var/bigfile/file.tar.gz"

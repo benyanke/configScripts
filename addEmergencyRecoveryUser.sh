cf5d1374305c515376b7b2da589abbecac8f08d4
Ben Yanke <ben@benyanke.com>
2017-03-14 11:52:26 -0500

    emergency recovery user setup script

---
+++ addEmergencyRecoveryUser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# add user with no password
+# create group for passwordless sudo ("sudonopw")
+# add passwordless sudo group
+# add notification system to ~/.ssh/rc
+# Upon login with key
+# Create login message and store somewhere, include in emergency user's bashrc
+  # Provide the following details in MOTD: 
+    # server hostname
+    # server role
+    # services installed
+    # server hosting provider
+    # which account of hosting provider
+
+# In notificaton, list:
+  # connecting ip
+  # connecting hostname
+  # datetime of connection
+  # Current hostname
+
+
+# When adding is complete, ping the mains erver and add to the central manifest, listing the hostname, IP, etc.

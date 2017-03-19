e7afe83f98a1033fc644b8a78f040a0e195c5c9e
Ben Yanke <ben@benyanke.com>
2017-03-19 14:52:04 -0500

    cups setup

---
+++ setupPiPrintServer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+printuser="benyanke";
+
+
+sudo cat /dev/null || exit 1;
+
+sudo apt-get update;
+sudo apt-get install cups -y;
+
+sudo usermod -aG lpadmin $printuser;
+
+# Set to listen on external port
+before="Listen localhost:631"
+after="Port 631"
+sudo sed -i -e 's/'$before'/'$after'/g' /etc/cups/cupsd.conf
+
+
+before="Order allow,deny"
+after="Order allow,deny\nAllow @local"
+sudo sed -i -e 's/'$before'/'$after'/g' /etc/cups/cupsd.conf
+
+# Restart
+sudo /etc/init.d/cups restart
+
+
+echo "Cups should now be set up!"

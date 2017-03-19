819fe05a3dc2591f37dbf85c04330ebb70cceec9
Ben Yanke <ben@benyanke.com>
2017-03-19 15:02:58 -0500

    update printserver script

--- setupPiPrintServer.sh
+++ setupPiPrintServer.sh
@@ -13,12 +13,12 @@ sudo usermod -aG lpadmin $printuser;
 # Set to listen on external port
 before="Listen localhost:631"
 after="Port 631"
-sudo sed -i -e 's/'$before'/'$after'/g' /etc/cups/cupsd.conf
+sudo sed -i -e 's/'"$before"'/'"$after"'/g' /etc/cups/cupsd.conf
 
 
 before="Order allow,deny"
 after="Order allow,deny\nAllow @local"
-sudo sed -i -e 's/'$before'/'$after'/g' /etc/cups/cupsd.conf
+sudo sed -i -e 's/'"$before"'/'"$after"'/g' /etc/cups/cupsd.conf
 
 # Restart
 sudo /etc/init.d/cups restart

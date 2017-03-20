568d50b132ebb176b02ca4e8833a8d1d5df63d26
Ben Yanke <ben@benyanke.com>
2017-03-19 21:27:07 -0500

    working on sane installer

--- setupPiScanServer.sh
+++ setupPiScanServer.sh
@@ -9,3 +9,15 @@ sudo apt-get install sane -y
 
 
 sudo sane-find-scanner
+
+
+before="RUN=no"
+after="RUN=yes"
+sudo sed -i -e 's/'"$before"'/'"$after"'/g' /etc/default/saned
+
+before="# data_portrange = 10000 - 10100"
+after="data_portrange = 10000 - 10100"
+sudo sed -i -e 's/'"$before"'/'"$after"'/g'  /etc/sane.d/saned.conf
+
+echo "10.10.10.0/24" | sudo tee -a /etc/sane.d/saned.conf
+

5b33a72ac0da3e5f3a6b3a3a4b0aa8c5ac6e8af1
Ben Yanke <ben@benyanke.com>
2017-04-10 12:46:26 -0500

    add wakeonlan

--- wakeOnLan.sh
+++ wakeOnLan.sh
@@ -8,10 +8,16 @@
 # Eth1:
 # 00:24:21:28:05:65
 
+# Pius-II (VM)
+# 52:54:00:2a:1e:8c
+
 # HomeServer
 # 00:24:7e:70:19:6f
 
 # Honorious-I
 # b8:ac:6f:8a:25:c8
+
+# RY Desktop
+# 18:03:73:E6:EF:42
 
 # wakeonlan

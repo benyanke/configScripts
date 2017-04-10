f83cd5845a8e01a33a0d8349f83ca3550b6c73db
Ben Yanke <ben@benyanke.com>
2017-04-10 12:40:42 -0500

    added wakeonlan

---
+++ wakeOnLan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pius-I
+# Bond:
+# 00:24:21:28:05:65
+# Eth0:
+# 00:24:21:28:05:65
+# Eth1:
+# 00:24:21:28:05:65
+
+# HomeServer
+# 00:24:7e:70:19:6f
+
+# Honorious-I
+# b8:ac:6f:8a:25:c8
+
+# wakeonlan

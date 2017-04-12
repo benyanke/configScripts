98254f3fd9457305ec08239e863f48ef5c002e83
Ben Yanke <ben@benyanke.com>
2017-04-12 10:44:51 -0500

    updated stress test

--- stresstest.sh
+++ stresstest.sh
@@ -2,15 +2,28 @@
 
 site="http://example.com"
 count="500" # Number of requests for each thread to make
-threads="10" # number of threads
+threads="100" # number of threads
+maxtime="20";
+
+sudo cat /dev/null || exit 1;
 
 COUNTER=0
 while [  $COUNTER -lt $threads ]; do
   echo "starting thread $COUNTER"
   curl -s -H 'Cache-Control: no-cache' "$site?thread=$COUNTER&[1-$count]"  > /dev/null &
+  disown;
   let COUNTER=COUNTER+1
 done
 
-
+# Time based kill
+sleep $maxtime &
 wait;
+sudo killall curl;
+sleep 0.5;
+sudo killall curl;
+sleep 0.5;
+sudo killall curl;
+sleep 0.5;
+sudo killall curl;
+
 echo "Stress test complete. Press enter to exit."

e6b43095fa61bac6ca78b1ce121212a15cd26dd6
Ben Yanke <ben@benyanke.com>
2017-05-26 22:52:37 -0500

    adding gnome setup

---
+++ gnomeSetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Gnome extensions to get
+
+
+# Main gnome shell integration
+# "gtile" "Window tiling manager"
+# Look at ubuntu survey
+# look at chrome extension
+# 
+
+
+
+
+###############################################################
+# sudo add-apt-repository ppa:ne0sight/chrome-gnome-shell
+# $ sudo apt-get update
+#$ sudo apt-get install chrome-gnome-shell
+
+

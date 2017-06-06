6f273a11cb9b4eb107f4d15c7cdc6ff51278d030
Ben Yanke <ben@benyanke.com>
2017-06-06 16:55:57 -0500

    updated plasma script

--- getPlasma.sh
+++ getPlasma.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update &&
 sudo add-apt-repository ppa:kubuntu-ppa/backports -y &&
-sudo apt-get update && sudo apt-get dist-upgrade &&
+sudo apt-get update && sudo apt-get dist-upgrade -y &&
 sudo apt-get install kubuntu-desktop -y

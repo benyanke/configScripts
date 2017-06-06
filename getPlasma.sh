5003625b7afe427e93d27bd7070ee9d927a0bbd4
Ben Yanke <ben@benyanke.com>
2017-06-06 16:53:21 -0500

    add plasma

---
+++ getPlasma.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update &&
+sudo add-apt-repository ppa:kubuntu-ppa/backports -y &&
+sudo apt-get update && sudo apt-get dist-upgrade &&
+sudo apt-get install kubuntu-desktop -y
